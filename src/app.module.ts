adaa931f2439859dfed319efa25985f83353fce4
NimansalaBandara <nimansaladamayanthibandara@gmail.com>
2025-04-25 10:43:05 +0530

    Create patien module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DoctorModule } from './doctor/doctor.module';
 import { MedichiveAdminModule } from './medichive-admin/medichive-admin.module';
 import { InstituteModule } from './institute/institute.module';
 import { ViewLabReportsModule } from './view-lab-reports/view-lab-reports.module';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [AppointmentModule, DoctorModule, MedichiveAdminModule, InstituteModule, ViewLabReportsModule],
+  imports: [AppointmentModule, DoctorModule, MedichiveAdminModule, InstituteModule, ViewLabReportsModule, PatientModule],
   controllers: [AppController],
   providers: [AppService],
 })
