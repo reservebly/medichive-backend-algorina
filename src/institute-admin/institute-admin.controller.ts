6a3aaaaba33c1f3ee5bddfe213c934954ee3efaf
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-07-17 15:58:35 +0530

    modify doctor manage routes

--- src/institute-admin/institute-admin.controller.ts
+++ src/institute-admin/institute-admin.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { InstituteAdminService } from './institute-admin.service';
 import { AddDoctorDto } from './dto/add-doctor.dto';
 import { DeleteDoctorDto } from './dto/delete-doctor.dto';
 import { AddInstituteAdminDto } from './dto/add-institute-admin.dto';
 import { AuthRoleGuard } from 'src/auth/guards/auth-role.guard';
 import { Roles } from 'src/auth/decorators/roles.decarator';
+import { AddPatientDto } from './dto/add-patient.dto';
 
 @Controller('institute-admin')
 @UseGuards(AuthRoleGuard)
@@ -18,11 +27,18 @@ export class InstituteAdminController {
     return await this.instituteAdminService.addDoctors(data);
   }
 
-  @Post('delete-doctor')
+  @Delete('delete-doctor')
   @Roles('INSTITUTE_ADMIN')
   @HttpCode(200)
   async deleteDoctor(@Body() data: DeleteDoctorDto) {
     return await this.instituteAdminService.deleteDoctor(data);
+  }
+
+  @Get('get-doctors')
+  @Roles('INSTITUTE_ADMIN')
+  @HttpCode(200)
+  async getDoctors() {
+    return await this.instituteAdminService.getDoctors();
   }
 
   @Post('add-institute-admin')
@@ -30,5 +46,12 @@ export class InstituteAdminController {
   @HttpCode(200)
   async addInstituteAdmin(@Body() data: AddInstituteAdminDto) {
     return await this.instituteAdminService.addInstituteAdmin(data);
+  }
+
+  @Post('add-patient')
+  @Roles()
+  @HttpCode(200)
+  async addPatient(@Body() data: AddPatientDto) {
+    return await this.instituteAdminService.addPatient(data);
   }
 }
